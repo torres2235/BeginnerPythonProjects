bd5131f7eb983b04fb256a8de3efb0371a4e6858
torres2235 <43655957+torres2235@users.noreply.github.com>
2022-07-19 17:37:32 -0700

    Console Tic-Tac-Toe Game

---
+++ tic-tac-toe_console/player.py
@@ -0,0 +1,43 @@
+import math
+import random
+
+class Player:
+        def __init__(self, letter):
+            # letter is x or o
+            self.letter = letter
+
+        #we want all players to get their next move given a game
+        def get_move(self, game):
+            pass
+
+#computer and player inhert from Player class
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        #get a random spot from our available moves
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8): ')
+            #we're going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, then we say it's invalid if
+            # that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True #if these are successful, the yay!
+            except ValueError:
+                print('Invald square. Try again.')
+
+        return val
